f750972b2300f13c0ca5b32908eb5d87478ef07f
leehyemi0468 <31530628+leehyemi0468@users.noreply.github.com>
2017-11-16 01:41:52 +0900

    Update initpost.sh

--- initpost.sh
+++ initpost.sh
@@ -46,7 +46,7 @@ POSTPATH="${BINPATH}/_posts"
 DIST_FOLDER="$POSTPATH"
 
 # Set your blog URL
-BLOG_URL="https://www.rossener.com/"
+BLOG_URL="https://leehyemi0468.github.io"
 
 # Set your assets URL
 ASSETS_URL="assets/img/"
